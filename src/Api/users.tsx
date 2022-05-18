d0693c66371424279ffb4767a00e2c45c4cf76a1
daibede <xuandai300092@gmail.com>
2022-05-18 10:13:15 +0700

    api users

--- src/Api/users.tsx
+++ src/Api/users.tsx
@@ -1,3 +1,26 @@
 import { UserType } from "../Types/UserType";
 import instance from "./instance";
 
+export const listUser = () => {
+    const url = `users` 
+    return instance.get(url)
+}
+export const readUser = (id: any) => {
+    const url = `users/${id}` 
+    return instance.get(url)
+}
+
+export const removeUser = (id: any) => {
+    const url = `users/${id}` 
+    return instance.delete(url)
+}
+
+export const addUser = (users: UserType) => {
+    const url = `users` 
+    return instance.post(url,users)
+}
+
+export const updateUser = (users: UserType) => {
+    const url = `users/${users._id}` 
+    return instance.put(url,users)
+}
